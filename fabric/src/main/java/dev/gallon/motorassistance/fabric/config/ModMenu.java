27ab7107aa92bff5932d1341ca2a2e6d228f2e20
Lilian Gallon <lilian.gallon@4sh.fr>
2026-04-30 09:15:01 +0200

    (chore): 26.1 migration (removed architectury)
    Based on https://github.com/jaredlll08/MultiLoader-Template

--- fabric/src/main/java/dev/gallon/motorassistance/fabric/config/ModMenu.java
+++ fabric/src/main/java/dev/gallon/motorassistance/fabric/config/ModMenu.java
@@ -2,7 +2,7 @@ package dev.gallon.motorassistance.fabric.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
@@ -11,6 +11,6 @@ public class ModMenu implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(TheModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(TheModConfig.class, parent).get();
     }
 }
